c589b8ea983c0285d6be917eba1c1dfddca28b0c
isaahyar <mohamadisa.ahyar@gmail.com>
2023-07-02 00:15:51 +0800

    New flutter project

---
+++ README.md
@@ -0,0 +1,7 @@
+# quotes_application
+
+A quotes application to display quotes with nice background images
+
+## Dependencies
+
+This project is made using carousel_slider, flutter_blurhash, flutter_spinkit and http to get image data from Unsplash API.
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:quotes_application/home_screen.dart';
+
+void main(){
+  runApp(const QuotesApp());
+}
+
+class QuotesApp extends StatelessWidget {
+  const QuotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen()
+    );
+  }
+}
